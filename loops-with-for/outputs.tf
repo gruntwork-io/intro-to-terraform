777c2e6f862b6f74727fc208744da4954839e581
Yevgeniy Brikman <brikis98@gmail.com>
2019-07-02 23:31:10 -0400

    Add loop-with-for example

---
+++ loops-with-for/outputs.tf
@@ -0,0 +1,15 @@
+output "upper_names" {
+  value = [for name in var.names : upper(name)]
+}
+
+output "short_upper_names" {
+  value = [for name in var.names : upper(name) if length(name) < 5]
+}
+
+output "bios" {
+  value = [for name, role in var.hero_thousand_faces : "${name} is the ${role}"]
+}
+
+output "upper_roles" {
+  value = {for name, role in var.hero_thousand_faces : upper(name) => upper(role)}
+}
---
+++ loops-with-for/variables.tf
@@ -0,0 +1,15 @@
+variable "names" {
+  description = "A list of names"
+  type        = list(string)
+  default     = ["neo", "trinity", "morpheus"]
+}
+
+variable "hero_thousand_faces" {
+  description = "map"
+  type        = map(string)
+  default     = {
+    neo      = "hero"
+    trinity  = "love interest"
+    morpheus = "mentor"
+  }
+}
