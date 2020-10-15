9c6dfc72edcc939b6ba9e2fb8b4d3d9a62b6f975
Yevgeniy Brikman <brikis98@gmail.com>
2020-10-15 15:58:21 +0100

    Require TF 0.12.26 everywhere

--- cluster-of-web-servers/main.tf
+++ cluster-of-web-servers/main.tf
@@ -6,11 +6,13 @@
 
 # ----------------------------------------------------------------------------------------------------------------------
 # REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
-# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
 # ----------------------------------------------------------------------------------------------------------------------
 
 terraform {
-  required_version = ">= 0.12"
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
 }
 
 # ------------------------------------------------------------------------------
