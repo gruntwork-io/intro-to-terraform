def24b899f24e3dba10be20fe262b58b8e4dc8b9
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2021-05-21 18:59:22 +0100

    Update to Terraform 0.15

--- loops-with-for/main.tf
+++ loops-with-for/main.tf
@@ -1,7 +1,7 @@
 terraform {
-  # This module is now only being tested with Terraform 0.14.x. However, to make upgrading easier, we are setting
+  # This module is now only being tested with Terraform 0.15.x. However, to make upgrading easier, we are setting
   # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
-  # forwards compatible with 0.14.x code.
+  # forwards compatible with 0.15.x code.
   required_version = ">= 0.12.26"
 }
 
