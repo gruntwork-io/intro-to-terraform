1f9a22ac7a65d8d24481c808430314d301dceba5
Rho <13165182+rhoboat@users.noreply.github.com>
2022-02-17 16:49:15 -0800

    Restrict provider version. (#22)

--- loops-with-for/main.tf
+++ loops-with-for/main.tf
@@ -3,6 +3,12 @@ terraform {
   # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
   # forwards compatible with 0.15.x code.
   required_version = ">= 0.12.26"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "< 4.0"
+    }
+  }
 }
 
 # NOTE: All the important code for this example is in outputs.tf.
